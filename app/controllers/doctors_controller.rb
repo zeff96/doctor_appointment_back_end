aacaa2e6f8fc5ba46ff3715afb40bca6ca2c8138
zeff96 <zeffo1975@gmail.com>
2023-07-14 07:21:00 +0300

    Add method for creating a new doctor

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,12 +1,27 @@
 class DoctorsController < ApplicationController
   before_action :set_doctor, only: %i[show destroy]
-  
+
   def index
     @doctors = Doctor.all
 
     render json: @doctors, each_serializer: DoctorSerializer
   end
 
+  def show; end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    if @doctor.save
+      render :show, status: :created, location: @doctor
+    else
+      render json: {error: @doctor.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
   
   private
 
