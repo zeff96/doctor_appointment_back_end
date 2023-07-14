d3c48e19d63eba739d9a6e815d0aee26f998f9e3
zeff96 <zeffo1975@gmail.com>
2023-07-14 07:10:18 +0300

    Add helper method for getting specific doctor

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,7 +1,16 @@
 class DoctorsController < ApplicationController
+  before_action :set_doctor, only: %i[show destroy]
+  
   def index
     @doctors = Doctor.all
 
     render json: @doctors, each_serializer: DoctorSerializer
+  end
+
+  
+  private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
   end
 end
