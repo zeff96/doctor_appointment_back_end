3928ed4d5eb3f9113ed3b060f812f6dca2b21329
zeff96 <zeffo1975@gmail.com>
2023-07-14 07:34:27 +0300

    Add doctor serializer

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,3 +1,11 @@
 class DoctorSerializer < ActiveModel::Serializer
-  attributes :id
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :bio, :image_url
+
+  def image_url
+    if object.image.attached?
+      url_for(object.image)
+    end
+  end
 end
