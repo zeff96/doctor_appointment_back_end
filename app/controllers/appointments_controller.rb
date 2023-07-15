91fbc7ee1db84e655d9fb89802e285c895071d68
zeff96 <zeffo1975@gmail.com>
2023-07-15 09:36:47 +0300

    Add controllers for appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,29 @@
 class AppointmentsController < ApplicationController
+  before_action :set_doctor, only: %i[new create index]
+
+  def index
+    @doctor.appointments
+  end
+
+  def new
+    @appointment = @doctor.appointments.build
+  end
+
+  def create
+    @appointment = @doctor.appointments.build(appointment_params)
+
+    if @appointment.save
+      render json: @appointment
+    else
+      render json: {error: @appointment.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @appointment.destroy
+    head :no_content
+  end
+
   private
 
   def set_doctor
