c918953f398dd07e4fc209eea87320a9ae945b54
zeff96 <zeffo1975@gmail.com>
2023-07-20 08:06:51 +0300

    Add test validations for social media

---
+++ spec/models/social_medium_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe SocialMedium, type: :model do
+  let(:user) {User.create(name: 'test', email: 'test@test.com', password: 'password')}
+  let(:image_path) {Rails.root.join('public', 'images', 'ani-kolleshi.jpg')}
+  let(:image_file) {File.open(image_path)}
+  let(:doctor_with_image) do
+    doctor = Doctor.new(
+      name: 'doc1',
+      bio: 'doc from kenya',
+      user: user
+    )
+    doctor.image.attach(io: image_file, filename: 'ani-kolleshi.jpg', content_type: 'image/jpeg')
+    doctor
+  end
+
+  describe 'validations' do
+    before do
+      @social_medium = doctor_with_image.build_social_medium(
+        facebook: 'https://facebook.com/doctorspage',
+        twitter: 'https://twitter.com/doctorspage',
+        instagram: 'https://instagram.com/doctorspage'
+      )
+    end
+
+    it 'should be valid' do
+      expect(@social_medium).to be_valid
+    end
+  end
+end
