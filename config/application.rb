7140f48223c1ff05f18086081fffc6c76ecded72
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-20 13:35:20 +0300

    Added new changes

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module DoctorAppoitmentApp
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.session_store :cookie_store, key: "interslice_session"
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
     config.api_only = true
   end
 end
