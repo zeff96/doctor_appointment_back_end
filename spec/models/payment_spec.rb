1810f03d855eea0c911712aaac944fefdc4ac6cf
zeff96 <zeffo1975@gmail.com>
2023-07-20 08:15:14 +0300

    Add payment validation test

---
+++ spec/models/payment_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Payment, type: :model do
+  let(:user) {User.create(name: 'test', email: 'test@test.com', password: 'password')}
+  let(:image_path) {Rails.root.join('public', 'images', 'ani-kolleshi.jpg')}
+  let(:image_file) {File.open(image_path)}
+  let(:doctor_with_image) do
+    doctor = Doctor.new(
+      name: 'doc1',
+      bio: 'doc from kenya',
+      user: user
+    )
+    doctor.image.attach(io: image_file, filename: 'ani-kolleshi.jpg', content_type: 'image/jpeg')
+    doctor
+  end
+
+  describe 'validations' do
+    before do
+      @payment = doctor_with_image.build_payment(
+        consultation_fee: 250
+      )
+    end
+
+    it 'should be valid' do
+      expect(@payment).to be_valid
+    end
+  end
+end
