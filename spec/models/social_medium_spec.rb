28a041cb1194fa2b6a242301e3f4188da20d6963
zeff96 <zeffo1975@gmail.com>
2023-07-20 08:09:13 +0300

    Add social media association test

--- spec/models/social_medium_spec.rb
+++ spec/models/social_medium_spec.rb
@@ -27,4 +27,11 @@ RSpec.describe SocialMedium, type: :model do
       expect(@social_medium).to be_valid
     end
   end
+
+  describe 'associations' do
+    it 'belongs to doctor' do
+      association = described_class.reflect_on_association(:doctor)
+      expect(association.macro).to eq :belongs_to
+    end
+  end
 end
