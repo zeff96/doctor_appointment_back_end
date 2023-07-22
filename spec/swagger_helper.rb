2e41bd928d1720c407b28df571929eadff56dcb8
zeff96 <zeffo1975@gmail.com>
2023-07-22 10:03:33 +0300

    Add doctor schema

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -38,6 +38,17 @@ RSpec.configure do |config|
               'Doctors'
             ],
             description: 'Returns a list of all doctors.',
+            parameters: [
+              {
+                name: 'id',
+                in: 'path',
+                required: true,
+                description: 'Doctor ID',
+                schema: {
+                  type: 'integer'
+                }
+              }
+            ],
             responses: {
               '200': {
                 description: 'Successful operation',
@@ -51,6 +62,160 @@ RSpec.configure do |config|
                     }
                   }
                 }
+              }
+            }
+          },
+          # post: {
+          #   summary: 'Create a new doctor',
+          #   tags: ['Posts'],
+          #   description: 'Creates a new doctor with the provided data.',
+          #   requestBody: {
+          #     required: true,
+          #     content: {
+          #       'application/json': {
+          #         schema: {
+          #           type: 'object',
+          #           properties: {
+          #             name: {
+          #               type: 'string'
+          #             },
+          #             bio: {
+          #               type: 'string',
+          #               format: 'text'
+          #             },
+          #             image: {
+          #               type: 'string',
+          #               format: 'binary'
+          #             },
+          #             location_attributes: {
+          #               type: 'object',
+          #               properties: {
+          #                 address: {
+          #                   type: 'string'
+          #                 },
+          #                 city: {
+          #                   type: 'string'
+          #                 },
+          #                 state: {
+          #                   type: 'string'
+          #                 },
+          #                 zip_code: {
+          #                   type: 'integer'
+          #                 }
+          #               }
+          #             },
+          #             payment_attributes: {
+          #               type: 'object',
+          #               properties: {
+          #                 consultation_fee: {
+          #                   type: 'decimal'
+          #                 }
+          #               }
+          #             },
+          #             social_media_attributes: {
+          #               type: 'object',
+          #               properties: {
+          #                 facebook: {
+          #                   type: 'string'
+          #                 },
+          #                 twitter: {
+          #                   type: 'string'
+          #                 },
+          #                 instagram: {
+          #                   type: 'string'
+          #                 }
+          #               }
+          #             }
+          #           },
+          #           required: ['name', 'bio', 'image', 'location_attributes', 'payment_attributes', 'social_media_attributes'] // Add any other required fields
+          #         }
+          #       }
+          #     }
+          #   },
+          #   responses: {
+          #     '201': {
+          #       description: 'Post created successfully',
+          #       content: {
+          #         'application/json': {
+          #           schema: {
+          #             type: 'array',
+          #             items: {
+          #               '$ref': '#/components/schemas/Doctor'
+          #             }
+          #           }
+          #         }
+          #       }
+          #     },
+          #     '400': {
+          #       description: 'Bad request - Invalid data',
+          #       content: {
+          #         'application/json': {
+          #           schema: {
+          #             type: 'object',
+          #             properties: {
+          #               error: {
+          #                 type: 'string'
+          #               }
+          #             }
+          #           }
+          #         }
+          #       }
+          #     }
+          #   }
+          # }
+        }
+      },
+      components: {
+        schemas: {
+          Doctor: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'integer'
+              },
+              name: {
+                type: 'string'
+              },
+              bio: {
+                type: 'string'
+              },
+              image: {
+                type: 'string'
+              },
+              location_attributes: {
+                '$ref': '#/components/schemas/Location'
+              },
+              social_media_attributes: {
+                '$ref': '#/components/schemas/SocialMedium'
+              },
+              payment_attributes: {
+                '$ref': '#/components/schemas/Payment'
+              }
+            }
+          },
+          DoctorInput: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                example: 'John Doe'
+              },
+              bio: {
+                type: 'string',
+                example: 'Experienced doctor with specialization in Cardiology'
+              },
+              image: {
+                type: 'string',
+                format: 'binary'
+              },
+              location_attributes: {
+                '$ref': '#/components/schemas/LocationInput'
+              },
+              social_media_attributes: {
+                '$ref': '#/components/schemas/SocialMediumInput'
+              },
+              payment_attributes: {
+                '$ref': '#/components/schemas/PaymentInput'
               }
             }
           },
