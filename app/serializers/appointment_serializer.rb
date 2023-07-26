3bde331748655f936c07811c5419fe1ee363313b
zeff96 <zeffo1975@gmail.com>
2023-07-27 02:09:16 +0300

    Add typo fix

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,4 +1,7 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :date, :city, :user_name
-  belongs_to :doctor, key: :doctor_name, serializer: DoctorNameSerializer
+  attributes :id, :date, :city, :user_name, :doctor_name
+
+  def doctor_name
+    object.doctor.name
+  end
 end
